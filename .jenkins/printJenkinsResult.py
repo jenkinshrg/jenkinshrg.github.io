918c01dc6bd1b565870025f55afd758f4a4747ca
jenkins hrg <jenkinshrg@gmail.com>
2015-06-30 14:57:26 +0900

    add first files

---
+++ .jenkins/printJenkinsResult.py
@@ -0,0 +1,126 @@
+import sys, os, json, urllib2
+from datetime import datetime
+
+print "---"
+print "layout: default"
+print "---"
+
+print "# Jenkins CI report For HRG"
+print "## Build and test report" + " (" + datetime.now().strftime("%Y/%m/%d %H:%M:%S") + ")"
+
+url = 'http://jenkinshrg:8080/job/' + sys.argv[1] + '/api/json?tree=color,lastCompletedBuild[result],builds[number,result,timestamp,duration,url]'
+r = urllib2.urlopen(url)
+root = json.loads(r.read())
+color = root['color']
+lastCompletedBuild = root['lastCompletedBuild']
+builds = root['builds']
+r.close()
+
+cnt = 0
+okcnt = 0.0
+for build in builds:
+    if build['result'] == "SUCCESS":
+        okcnt += 1
+    cnt += 1
+#    if cnt == 10:
+#        break
+stability = int((okcnt / cnt) * 100)
+if stability >= 80:
+    iconUrl = "health-80plus.png"
+elif stability >= 60:
+    iconUrl = "health-60to79.png"
+elif stability >= 40:
+    iconUrl = "health-40to59.png"
+elif stability >= 20:
+    iconUrl = "health-20to39.png"
+else:
+    iconUrl = "health-00to19.png"
+
+print "### Build Status"
+print "___"
+print "![Build Status](http://jenkinshrg.github.io/badge.svg)"
+print "  "
+
+#print "### Build Status"
+#print "___"
+#print "![Jenkins Icon](http://jenkinshrg.github.io/images/48x48/"+ color + ".png)"
+#print lastCompletedBuild['result']
+#print "  "
+
+print "### Build Stability"
+print "___"
+print "![Jenkins Icon](http://jenkinshrg.github.io/images/48x48/" + iconUrl + ")"
+print str(stability) + "%"
+print "  "
+
+#print "### Build Trend"
+#print "___"
+#print "* gui test(choreonoid)"
+#print "  "
+#print "![Test Trend](http://jenkinshrg.github.io/test.png)"
+#print "  "
+#print "* code counter(cccc)"
+#print "  "
+#print "![Cccc Trend](http://jenkinshrg.github.io/cccc.png)"
+#print "  "
+#print "* code checker(cppcheck)"
+#print "  "
+#print "![Cppcheck Trend](http://jenkinshrg.github.io/cppcheck.png)"
+#print "  "
+
+print "### Build History"
+print "___"
+print "|Status|Time|Duration|Changes|Build|Test|Note|"
+print "|---|---|---|---|---|---|---|"
+
+cnt = 0
+for build in builds:
+    result = build['result']
+    if result == "SUCCESS":
+        color = "blue"
+    elif result == "UNSTABLE":
+        color = "yellow"
+    elif result == "FAILURE":
+        color = "red"
+    else:
+        color = "red"
+    errorDetails = ""
+    errorStackTrace = ""
+    if result == "UNSTABLE":
+        url = build['url'] + 'testReport/api/json?tree=suites[cases[errorDetails,errorStackTrace]]'
+        r = urllib2.urlopen(url)
+        root = json.loads(r.read())
+        errorDetails = root['suites'][0]['cases'][0]['errorDetails']
+        errorStackTrace = root['suites'][0]['cases'][0]['errorStackTrace']
+        cause = "(" + errorDetails + "/" + errorStackTrace + ")"
+    path = "/var/lib/jenkins/jobs/" + sys.argv[1] + "/builds/" + str(build['number']) + "/archive/testbed-terrain.url"
+    link1 = ""
+    link2 = ""
+    link3 = ""
+    if os.path.exists(path) :
+        f = open(path)
+        line = f.readline()
+        while line:
+            if "BUILD" in line:
+                tag = "BUILD"
+            elif "CHANGES" in line:
+                tag = "CHANGES"
+            elif "CONSOLE" in line:
+                tag = "CONSOLE"
+            elif "IMAGE" in line:
+                tag = "IMAGE"
+            elif "VIDEO" in line:
+                tag = "VIDEO"
+            else:
+                if tag == "CHANGES":
+                    link1 = "[" + str(tag) + "](" + line.strip() + ")" + " "
+                elif tag == "BUILD":
+                    link2 = "[" + str(tag) + "](" + line.strip() + ")" + " "
+                else:
+                    link3 += "[" + str(tag) + "](" + line.strip() + ")" + " "
+            line = f.readline()
+        f.close()
+    print "|" + "![Jenkins Icon](http://jenkinshrg.github.io/images/24x24/"+ color + ".png)" + str(result) + "|" + str(datetime.fromtimestamp(build['timestamp'] / 1000).strftime("%Y/%m/%d %H:%M")) + "|" + str(build['duration'] / 60 / 1000) + " min." + "|" + str(link1) + "|" + str(link2) + "|" + str(link3) + "|" + str(errorDetails) + " " + str(errorStackTrace) + "|"
+    cnt += 1
+#    if cnt == 10:
+#        break
---
+++ .jenkins/report.sh
@@ -0,0 +1,33 @@
+if [ ! -e images ]; then
+    mkdir -p images
+    sudo cp /var/cache/jenkins/war/images/headshot.png images
+    mkdir -p images/48x48
+    sudo cp /var/cache/jenkins/war/images/48x48/nobuilt.png images/48x48
+    sudo cp /var/cache/jenkins/war/images/48x48/blue.png images/48x48
+    sudo cp /var/cache/jenkins/war/images/48x48/red.png images/48x48
+    sudo cp /var/cache/jenkins/war/images/48x48/yellow.png images/48x48
+    sudo cp /var/cache/jenkins/war/images/48x48/aborted.png images/48x48
+    sudo cp /var/cache/jenkins/war/images/48x48/disabled.png images/48x48
+    sudo cp /var/cache/jenkins/war/images/48x48/health-00to19.png images/48x48
+    sudo cp /var/cache/jenkins/war/images/48x48/health-20to39.png images/48x48
+    sudo cp /var/cache/jenkins/war/images/48x48/health-40to59.png images/48x48
+    sudo cp /var/cache/jenkins/war/images/48x48/health-60to79.png images/48x48
+    sudo cp /var/cache/jenkins/war/images/48x48/health-80plus.png images/48x48
+    mkdir -p images/24x24
+    sudo cp /var/cache/jenkins/war/images/24x24/nobuilt.png images/24x24
+    sudo cp /var/cache/jenkins/war/images/24x24/blue.png images/24x24
+    sudo cp /var/cache/jenkins/war/images/24x24/red.png images/24x24
+    sudo cp /var/cache/jenkins/war/images/24x24/yellow.png images/24x24
+    sudo cp /var/cache/jenkins/war/images/24x24/aborted.png images/24x24
+    sudo cp /var/cache/jenkins/war/images/24x24/disabled.png images/24x24
+fi
+
+wget -q -O badge.svg http://jenkinshrg:8080/job/update/badge/icon
+wget -q -O test.png http://jenkinshrg:8080/job/update/test/trend
+wget -q -O cccc.png http://jenkinshrg:8080/job/inspection/ccccResult/graph
+wget -q -O cppcheck.png http://jenkinshrg:8080/job/inspection/cppcheckResult/graph
+python .jenkins/printJenkinsResult.py update > index.md
+
+git add --all
+git commit -m "update report"
+git push origin master
