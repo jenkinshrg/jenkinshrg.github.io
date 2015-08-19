7a5e3f4ac070a1a033fcfa2cb30afcdf1dbff0d4
jenkinshrg <jenkinshrg@gmail.com>
2015-08-19 16:54:10 +0900

    modify script

--- .jenkins.sh
+++ .jenkins.sh
@@ -7,6 +7,11 @@ mkdir -p ${REPORT_JOB}
 wget -q -O ${REPORT_JOB}/badge.svg http://localhost:8080/job/${REPORT_JOB}/badge/icon
 python printJenkinsResult.py ${REPORT_JOB} > ${REPORT_JOB}/index.md
 
+REPORT_JOB=drcutil-desktop
+mkdir -p ${REPORT_JOB}
+wget -q -O ${REPORT_JOB}/badge.svg http://localhost:8080/job/${REPORT_JOB}/badge/icon
+python printJenkinsResult.py ${REPORT_JOB} > ${REPORT_JOB}/index.md
+
 REPORT_JOB=drcutil-64
 mkdir -p ${REPORT_JOB}
 wget -q -O ${REPORT_JOB}/badge.svg http://localhost:8080/job/${REPORT_JOB}/badge/icon
