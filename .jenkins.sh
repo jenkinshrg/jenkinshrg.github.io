39be12dce87ef778e99e43cf9bd10ac3c06c8bd5
jenkinshrg <jenkinshrg@gmail.com>
2016-01-07 13:54:24 +0900

    modify script

--- .jenkins.sh
+++ .jenkins.sh
@@ -3,24 +3,28 @@
 python printJenkinsResultSummary.py > index.md
 
 REPORT_JOB=drcutil
-mkdir -p ${REPORT_JOB}
-wget -q -O ${REPORT_JOB}/badge.svg http://localhost:8080/job/${REPORT_JOB}/badge/icon
-python printJenkinsResult.py ${REPORT_JOB} > ${REPORT_JOB}/index.md
+#mkdir -p ${REPORT_JOB}
+#wget -q -O ${REPORT_JOB}/badge.svg http://localhost:8080/job/${REPORT_JOB}/badge/icon
+#python printJenkinsResult.py ${REPORT_JOB} > ${REPORT_JOB}/index.md
+python printJenkinsResult.py ${REPORT_JOB} >> index.md
 
 REPORT_JOB=drcutil-desktop
-mkdir -p ${REPORT_JOB}
-wget -q -O ${REPORT_JOB}/badge.svg http://localhost:8080/job/${REPORT_JOB}/badge/icon
-python printJenkinsResult.py ${REPORT_JOB} > ${REPORT_JOB}/index.md
+#mkdir -p ${REPORT_JOB}
+#wget -q -O ${REPORT_JOB}/badge.svg http://localhost:8080/job/${REPORT_JOB}/badge/icon
+#python printJenkinsResult.py ${REPORT_JOB} > ${REPORT_JOB}/index.md
+python printJenkinsResult.py ${REPORT_JOB} >> index.md
 
 REPORT_JOB=drcutil-64
-mkdir -p ${REPORT_JOB}
-wget -q -O ${REPORT_JOB}/badge.svg http://localhost:8080/job/${REPORT_JOB}/badge/icon
-python printJenkinsResult.py ${REPORT_JOB} > ${REPORT_JOB}/index.md
+#mkdir -p ${REPORT_JOB}
+#wget -q -O ${REPORT_JOB}/badge.svg http://localhost:8080/job/${REPORT_JOB}/badge/icon
+#python printJenkinsResult.py ${REPORT_JOB} > ${REPORT_JOB}/index.md
+python printJenkinsResult.py ${REPORT_JOB} >> index.md
 
 REPORT_JOB=drcutil-32
-mkdir -p ${REPORT_JOB}
-wget -q -O ${REPORT_JOB}/badge.svg http://localhost:8080/job/${REPORT_JOB}/badge/icon
-python printJenkinsResult.py ${REPORT_JOB} > ${REPORT_JOB}/index.md
+#mkdir -p ${REPORT_JOB}
+#wget -q -O ${REPORT_JOB}/badge.svg http://localhost:8080/job/${REPORT_JOB}/badge/icon
+#python printJenkinsResult.py ${REPORT_JOB} > ${REPORT_JOB}/index.md
+python printJenkinsResult.py ${REPORT_JOB} >> index.md
 
 git add --all
 git commit -m "update report"
