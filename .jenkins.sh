22091039d7567c0ee2b7a09c2bf8be273dd511c3
jenkinshrg <jenkinshrg@gmail.com>
2016-01-29 11:44:10 +0900

    modify script

--- .jenkins.sh
+++ .jenkins.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-URL=http://jenkinshrg
+JENKINS_URL=http://jenkinshrg.a01.aist.go.jp
 
-wget -q -O drcutil-64.png ${URL}/job/drcutil-64/test/trend
-wget -q -O drcutil-desktop.png ${URL}/job/drcutil-desktop/test/trend
-python printJenkinsResultSummary.py ${URL} > index.md
+wget -q -O drcutil-64.png ${JENKINS_URL}/job/drcutil-64/test/trend
+wget -q -O drcutil-desktop.png ${JENKINS_URL}/job/drcutil-desktop/test/trend
+python printJenkinsResultSummary.py ${JENKINS_URL} > index.md
 
 REPORT_JOB=drcutil-32
-wget -q -O ${REPORT_JOB}.svg ${URL}/job/${REPORT_JOB}/badge/icon
-python printJenkinsResult.py ${REPORT_JOB} ${URL} >> index.md
+wget -q -O ${REPORT_JOB}.svg ${JENKINS_URL}/job/${REPORT_JOB}/badge/icon
+python printJenkinsResult.py ${REPORT_JOB} ${JENKINS_URL} >> index.md
 
 REPORT_JOB=drcutil-64
-wget -q -O ${REPORT_JOB}.svg ${URL}/job/${REPORT_JOB}/badge/icon
-python printJenkinsResult.py ${REPORT_JOB} ${URL} >> index.md
+wget -q -O ${REPORT_JOB}.svg ${JENKINS_URL}/job/${REPORT_JOB}/badge/icon
+python printJenkinsResult.py ${REPORT_JOB} ${JENKINS_URL} >> index.md
 
 REPORT_JOB=drcutil-desktop
-wget -q -O ${REPORT_JOB}.svg ${URL}/job/${REPORT_JOB}/badge/icon
-python printJenkinsResult.py ${REPORT_JOB} ${URL} >> index.md
+wget -q -O ${REPORT_JOB}.svg ${JENKINS_URL}/job/${REPORT_JOB}/badge/icon
+python printJenkinsResult.py ${REPORT_JOB} ${JENKINS_URL} >> index.md
 
 REPORT_JOB=drcutil-task-terrain
-wget -q -O ${REPORT_JOB}.svg ${URL}/job/${REPORT_JOB}/badge/icon
-python printJenkinsResult.py ${REPORT_JOB} ${URL} >> index.md
+wget -q -O ${REPORT_JOB}.svg ${JENKINS_URL}/job/${REPORT_JOB}/badge/icon
+python printJenkinsResult.py ${REPORT_JOB} ${JENKINS_URL} >> index.md
 
 git add --all
 git commit -m "update report"
