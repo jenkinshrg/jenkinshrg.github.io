4c1d06c81dfc8f84988423dd4101d4db9fd50d8a
jenkinshrg <jenkinshrg@gmail.com>
2016-01-29 11:18:57 +0900

    modify script

--- .jenkins.sh
+++ .jenkins.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-wget -q -O drcutil-64.png http://localhost:8080/job/drcutil-64/test/trend
-wget -q -O drcutil-desktop.png http://localhost:8080/job/drcutil-desktop/test/trend
+URL=http://jenkinshrg
+
+wget -q -O drcutil-64.png ${URL}/job/drcutil-64/test/trend
+wget -q -O drcutil-desktop.png ${URL}/job/drcutil-desktop/test/trend
 python printJenkinsResultSummary.py > index.md
 
 REPORT_JOB=drcutil-32
-wget -q -O ${REPORT_JOB}.svg http://localhost:8080/job/${REPORT_JOB}/badge/icon
+wget -q -O ${REPORT_JOB}.svg ${URL}/job/${REPORT_JOB}/badge/icon
 python printJenkinsResult.py ${REPORT_JOB} >> index.md
 
 REPORT_JOB=drcutil-64
-wget -q -O ${REPORT_JOB}.svg http://localhost:8080/job/${REPORT_JOB}/badge/icon
+wget -q -O ${REPORT_JOB}.svg ${URL}/job/${REPORT_JOB}/badge/icon
 python printJenkinsResult.py ${REPORT_JOB} >> index.md
 
 REPORT_JOB=drcutil-desktop
-wget -q -O ${REPORT_JOB}.svg http://localhost:8080/job/${REPORT_JOB}/badge/icon
+wget -q -O ${REPORT_JOB}.svg ${URL}/job/${REPORT_JOB}/badge/icon
 python printJenkinsResult.py ${REPORT_JOB} >> index.md
 
 git add --all
