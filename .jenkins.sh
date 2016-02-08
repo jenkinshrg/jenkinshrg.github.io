4fcf4f06816609cc20d75e86e89bdf9e7f6d0033
jenkinshrg <jenkinshrg@gmail.com>
2016-02-08 12:28:07 +0900

    modify script

--- .jenkins.sh
+++ .jenkins.sh
@@ -2,17 +2,17 @@ JENKINS_URL=http://jenkinshrg.a01.aist.go.jp/
 
 python printJenkinsResultSummary.py ${JENKINS_URL} > index.md
 
-REPORT_JOB=drcutil-build-32
-wget -q -O ${REPORT_JOB}.svg ${JENKINS_URL}/job/${REPORT_JOB}/badge/icon
-python printJenkinsResult.py ${REPORT_JOB} ${JENKINS_URL} >> index.md
+#REPORT_JOB=drcutil-build-32
+#wget -q -O ${REPORT_JOB}.svg ${JENKINS_URL}/job/${REPORT_JOB}/badge/icon
+#python printJenkinsResult.py ${REPORT_JOB} ${JENKINS_URL} >> index.md
 
-REPORT_JOB=drcutil-build-64
-wget -q -O ${REPORT_JOB}.svg ${JENKINS_URL}/job/${REPORT_JOB}/badge/icon
-python printJenkinsResult.py ${REPORT_JOB} ${JENKINS_URL} >> index.md
+#REPORT_JOB=drcutil-build-64
+#wget -q -O ${REPORT_JOB}.svg ${JENKINS_URL}/job/${REPORT_JOB}/badge/icon
+#python printJenkinsResult.py ${REPORT_JOB} ${JENKINS_URL} >> index.md
 
-REPORT_JOB=drcutil-inspection
-wget -q -O ${REPORT_JOB}.svg ${JENKINS_URL}/job/${REPORT_JOB}/badge/icon
-python printJenkinsResult.py ${REPORT_JOB} ${JENKINS_URL} >> index.md
+#REPORT_JOB=drcutil-inspection
+#wget -q -O ${REPORT_JOB}.svg ${JENKINS_URL}/job/${REPORT_JOB}/badge/icon
+#python printJenkinsResult.py ${REPORT_JOB} ${JENKINS_URL} >> index.md
 
 REPORT_JOB=drcutil-task-balancebeam
 wget -q -O ${REPORT_JOB}.svg ${JENKINS_URL}/job/${REPORT_JOB}/badge/icon
@@ -34,9 +34,9 @@ REPORT_JOB=drcutil-task-wall
 wget -q -O ${REPORT_JOB}.svg ${JENKINS_URL}/job/${REPORT_JOB}/badge/icon
 python printJenkinsResult.py ${REPORT_JOB} ${JENKINS_URL} >> index.md
 
-REPORT_JOB=drcutil-test
-wget -q -O ${REPORT_JOB}.svg ${JENKINS_URL}/job/${REPORT_JOB}/badge/icon
-python printJenkinsResult.py ${REPORT_JOB} ${JENKINS_URL} >> index.md
+#REPORT_JOB=drcutil-test
+#wget -q -O ${REPORT_JOB}.svg ${JENKINS_URL}/job/${REPORT_JOB}/badge/icon
+#python printJenkinsResult.py ${REPORT_JOB} ${JENKINS_URL} >> index.md
 
 git add --all
 git commit -m "update report"
